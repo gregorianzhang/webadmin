f358a40e197241bc8fe0a73da3f0d13c209eb35d
gregorian <gregorian.zhang@gmail.com>
2013-10-08 18:31:21 +0800

    添加基本模板文件

--- control/views.py
+++ control/views.py
@@ -1,11 +1,16 @@
 # Create your views here.
+# -*- coding: utf-8 -*-
 
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
 
 def home(request):
     #if request.user.is_authenticated():
-        #return render_to_response("account_base.html")
-    return HttpResponse("<html>Hello world</html>")
+    para = {"title": "hello world", "current_date": "哦哦哦哦饿", "content": "aa bb cc dd ee", "keywords": "django fabric webadmin", "description": "aaa sss dddd"}
+
+    return render_to_response("index.html", para)
+    #return HttpResponse("<html>Hello world</html>")
     #else:
     #    return HttpResponseRedirect("/error")
 
