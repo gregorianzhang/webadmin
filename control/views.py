348adad4504e97db8a9267e9fc0dc76801ef9dc7
gregorian <gregorian.zhang@gmail.com>
2013-10-11 18:37:18 +0800

    修改基本的数据

--- control/views.py
+++ control/views.py
@@ -10,7 +10,7 @@ def home(request):
     #if request.user.is_authenticated():
     #ip = machine.objects.raw('select * from control_machine')
     iplist = machine.objects.all()
-    cmdlist = machine.objects.all()
+    cmdlist = cmd.objects.all()
     #ip = machine.object.values("ip").all
     para = { "iplist":iplist, "title": "home" ,"cmdlist":cmdlist}
 #    para = {"title": "hello world", "current_date": "哦哦哦哦饿", "content": "aa bb cc dd ee", "keywords": "django fabric webadmin", "description": "aaa sss dddd"}
