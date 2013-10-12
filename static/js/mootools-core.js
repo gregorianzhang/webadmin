c1fc0d0a43a6f916a6d293f08b3fdd3126804865
gregorian <gregorian.zhang@gmail.com>
2013-10-12 18:20:15 +0800

    测试js

---
+++ static/js/mootools-core.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    
+var aa = $('<p>eeeee</p>');
+$("h2").append(aa);
+alert("2323");
+alert($(".iplist table tbody tr td").text());
+$("span").text("599.99");
+    
+}
+);
--- webadmin/settings.py
+++ webadmin/settings.py
@@ -51,7 +51,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/home/roy/PycharmProjects/webadmin/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -112,7 +112,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "./templates",
+    "/home/roy/PycharmProjects/webadmin/templates",
 )
 
 INSTALLED_APPS = (
